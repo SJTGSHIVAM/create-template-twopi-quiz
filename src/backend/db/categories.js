53467a11b5b918b1468b93ed2c104ddfe97c90ef
sjtgshivam <getlovebymagicalshivam@gmail.com>
2022-05-22 05:59:05 +0530

    Added: dummy db values

---
+++ src/backend/db/categories.js
@@ -0,0 +1,41 @@
+export const categories = [
+  {
+    id: "11",
+    title: "HTML",
+    description: "this is category for html",
+    image: "/gvh/ghvg",
+    category_type: "FEATURED",
+    tags: ["HTML"],
+    timer_detail: [
+      { level: "EASY", time: 30 },
+      { level: "MED", time: 60 },
+      { level: "HARD", time: 90 },
+    ],
+  },
+  {
+    id: "12",
+    title: "HTML",
+    description: "this is category for html",
+    image: "/gvh/ghvg",
+    category_type: "PERSONAL",
+    tags: ["HTML"],
+    timer_detail: [
+      { level: "EASY", time: 30 },
+      { level: "MED", time: 70 },
+      { level: "HARD", time: 90 },
+    ],
+  },
+  {
+    id: "123",
+    title: "Js",
+    description: "this is category for html",
+    image: "/gvh/ghvg",
+    category_type: "FEATURED",
+    tags: ["HTML"],
+    timer_detail: [
+      { level: "EASY", time: 30 },
+      { level: "MED", time: 70 },
+      { level: "HARD", time: 90 },
+    ],
+  },
+];
---
+++ src/backend/db/highscores.js
@@ -0,0 +1,62 @@
+export const highscores = [
+  {
+    id: "174",
+    rank: 10,
+    username: "sjtgshivam",
+    score: 50,
+  },
+  {
+    id: "19",
+    rank: 9,
+    username: "RajV",
+    score: 60,
+  },
+  {
+    id: "18",
+    rank: 8,
+    username: "rlax",
+    score: 70,
+  },
+  {
+    id: "17",
+    rank: 7,
+    username: "shws",
+    score: 80,
+  },
+  {
+    id: "12",
+    rank: 6,
+    username: "vks",
+    score: 90,
+  },
+  {
+    id: "123",
+    rank: 5,
+    username: "drvs",
+    score: 100,
+  },
+  {
+    id: "1232",
+    rank: 4,
+    username: "deep",
+    score: 110,
+  },
+  {
+    id: "23112",
+    rank: 3,
+    username: "Shiv",
+    score: 120,
+  },
+  {
+    id: "13522",
+    rank: 2,
+    username: "Shivi",
+    score: 130,
+  },
+  {
+    id: "13112",
+    rank: 1,
+    username: "Aru",
+    score: 140,
+  },
+];
